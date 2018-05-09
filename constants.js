9aa8ae73021cf6d3bba71ab42b59594ba94821b7
vrian <junlynbryanalburo@yahoo.com>
2018-05-10 02:00:26 +0800

    Updated Firebase constants

---
+++ constants.js
@@ -0,0 +1,8 @@
+export const APP_NAME = 'RN-starter';
+
+export const FIREBASE_API_KEY = "AIzaSyDHPo_A3OKTv3zjnf1DDr6NF2lFgsRWGJY";
+export const FIREBASE_AUTH_DOMAIN = "uvmw2018.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://uvmw2018.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "uvmw2018";
+export const FIREBASE_STORAGE_BUCKET = "uvmw2018.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "593975349444";
