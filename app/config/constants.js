2553b195f343618207118f76b1b47163f8a226bf
JoshCrusader <joshua_cruzada@dlsu.edu.ph>
2018-05-17 16:30:05 +0800

    change firebase configurations

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,8 +1,8 @@
-export const APP_NAME = 'UnivWeek';
+export const APP_NAME = 'AHRO';
 
-export const FIREBASE_API_KEY = "AIzaSyDWB0WM6XO32yFlbESShIJBmZAlp0YlC7I";
-export const FIREBASE_AUTH_DOMAIN = "univweek.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://univweek.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "univweek";
-export const FIREBASE_STORAGE_BUCKET = "univweek.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "616642820373";
+export const FIREBASE_API_KEY = "AIzaSyDHPo_A3OKTv3zjnf1DDr6NF2lFgsRWGJY";
+export const FIREBASE_AUTH_DOMAIN = "uvmw2018.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://uvmw2018.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "uvmw2018";
+export const FIREBASE_STORAGE_BUCKET = "uvmw2018.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "593975349444";
