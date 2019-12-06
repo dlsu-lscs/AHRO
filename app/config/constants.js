93a1969b7b07f0c735de0348c8442064a2406c3d
JoshCrusader <joshua_cruzada@dlsu.edu.ph>
2019-12-06 23:31:03 +0800

    Delete constants.js

--- app/config/constants.js
+++
@@ -1,8 +0,0 @@
-export const APP_NAME = 'AHRO';
-
-export const FIREBASE_API_KEY = "AIzaSyDHPo_A3OKTv3zjnf1DDr6NF2lFgsRWGJY";
-export const FIREBASE_AUTH_DOMAIN = "uvmw2018.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://uvmw2018.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "uvmw2018";
-export const FIREBASE_STORAGE_BUCKET = "uvmw2018.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "593975349444";
