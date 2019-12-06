be4fd106baf0ee26707b5a4739df0fccfee836c8
JoshCrusader <joshua_cruzada@dlsu.edu.ph>
2019-12-06 23:32:26 +0800

    Delete constants.js

--- app/config/constants.js
+++
@@ -1,8 +0,0 @@
-export const APP_NAME = 'UnivWeek';
-
-export const FIREBASE_API_KEY = "AIzaSyDWB0WM6XO32yFlbESShIJBmZAlp0YlC7I";
-export const FIREBASE_AUTH_DOMAIN = "univweek.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://univweek.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "univweek";
-export const FIREBASE_STORAGE_BUCKET = "univweek.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "616642820373";
